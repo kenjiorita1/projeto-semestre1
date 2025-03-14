2ddf5699e4e564f37d9077f526673d42a8e33054
Kenji-orita <Fernando.orita@fatec.sp.gov.br>
2025-03-13 22:37:32 -0300

    feat: criado rota de get e post e delete

--- src/main/java/com/fatec/projeto/projeto2025/controllers/ClienteController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ClienteController.java
@@ -10,17 +10,47 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/cliente")
 public class ClienteController {
+        
         private static final Logger logger = LoggerFactory.getLogger(ClienteController.class.getName());
 
+        private final List<Cliente> clientes = new ArrayList<>();
+
+        private Long idCount = 1L ;
+
+
     //http://localhost:8080/api/cliente/criarCliente => POST
-    @PostMapping("/criarCliente")
+    @PostMapping("/criarCliente") 
     public String CriarCliente(@RequestBody Cliente cliente) {
+        cliente.setId(idCount + 1);
+        clientes.add(cliente);
+
         logger.info("Recebido JSON: Nome={}, Idade={}", cliente.getNome(), cliente.getIdade());
         return "O Cliente "+cliente.getNome()+ " de idade "+cliente.getIdade()+" foi criado";
     }
+
+    @GetMapping("/clientes")
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    @DeleteMapping("/cliente/{id}")
+    public String ClienteDelete(@PathVariable Long id){
+        for(Cliente cliente: clientes ){
+            if(cliente.getId().equals(id)){
+                clientes.remove(cliente);
+                return "cliente deletado com sucesso!";
+            }
+        }
+         return "cliente não encontrado!";
+    }
+    
 }
